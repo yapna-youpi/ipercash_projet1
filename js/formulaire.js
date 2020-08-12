7458e1241601b2e13660c15f6926a55554cff307
Yapna-youpi <yapnayoupi@gmail.com>
2020-08-12 11:06:06 +0100

    update

--- js/formulaire.js
+++ js/formulaire.js
@@ -3,41 +3,22 @@ document.getElementById('btn').addEventListener("click",function(e){
  var reqmail = document.getElementById("email").value;
  var reqsms = document.getElementById("message").value;
  var erreur;
-
+ document.getElementById("form").reset();
    if((!reqnom || !reqmail) ||  !reqsms){
-      var erreur = "veuillez remplir les champs";
-      }
-   if(erreur){
-      document.getElementById("comments").innerHTML = erreur;
-      return(false);
-   }else{
-         while(addEventListener("onclick",function resetForm(n1,n2,n3) {
-            reset(n1);
-            reset(n2);
-            reset(n3);
-            
-         })){
-            var nom = document.getElementById("nom"),email=document.getElementById("email"),sms=document.getElementById("comments");
-
-            resetForm(nom,email,sms);
-
+         var erreur = "veuillez remplir tous les champs!";
          }
-         
-         
-         
-         document.getElementById("comments").innerHTML = reqsms;
-         e.preventDefault;
-        var elt_ajout = document.createElement("div");
-        var text_ajout = document.createTextNode(reqsms);
-        
-
-
-
-      }   
-   
-   
-   
-   
+      if(erreur){
+         document.getElementById("comments").innerHTML = erreur;
+         return(false);
+      } else{
+            const div = document.createElement("div");
+            div.innerHTML = reqsms;
+            const comment = document.getElementById("comment2");
+            comment.appendChild(div);
+            
+           
+           
+         }  
    
 });
 
